d3054f973f0be3ff9b922040cdfa91fe865914b0
denilson-souza <denilson.souza@mais-resultados.com>
2020-06-10 00:22:17 -0300

    update menu items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import React from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, Switch } from "react-router-dom";
 import "./App.css";
-// import HomePage from "./pages/homepage/homepage.component";
-import "./pages/homepage/homepage.styles.scss";
+import HomePage from "./pages/homepage/homepage.component";
 
-const HomePage = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <button onClick={() => props.history.push("/topics")}>Topics</button>
-      <h1>HOME PAGE</h1>
-    </div>
-  );
-};
-
-const TopicsList = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-    </div>
-  );
-};
-
-const TopicDetail = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-    </div>
-  );
-};
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/topics" component={TopicsList} />
-      <Route path="/topics/:topicId" component={TopicDetail} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,18 +13,21 @@ class Directory extends React.Component {
           imageUrl:
             "https://images.unsplash.com/photo-1542060748-10c28b62716f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
           id: 1,
+          linkUrl: "hats",
         },
         {
           title: "calças",
           imageUrl:
             "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
           id: 2,
+          linkUrl: "",
         },
         {
           title: "sapatos",
           imageUrl:
             "https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
           id: 3,
+          linkUrl: "",
         },
         {
           title: "moletons",
@@ -32,6 +35,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1537274942065-eda9d00a6293?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
           size: "large",
           id: 4,
+          linkUrl: "",
         },
         {
           title: "bermudas",
@@ -39,6 +43,7 @@ class Directory extends React.Component {
             "https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
           size: "large",
           id: 5,
+          linkUrl: "",
         },
       ],
     };
@@ -47,8 +52,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} image={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
